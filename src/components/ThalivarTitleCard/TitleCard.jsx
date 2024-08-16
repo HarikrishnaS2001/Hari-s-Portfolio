a3eb88dfde7793469218ac7b2bbe8f3426e37759
Hari <harikrishna.sathiyanarayaynan@bounteous.com>
2024-08-17 02:08:58 +0530

    TitleCard improved

--- src/components/ThalivarTitleCard/TitleCard.jsx
+++ src/components/ThalivarTitleCard/TitleCard.jsx
@@ -3,7 +3,7 @@ import "./TitleCard.css";
 import { motion } from "framer-motion";
 
 const TitleCard = () => {
-  const line1 = ["F", "U", "L", "L", "S", "T", "A", "C", "K"];
+  const line1 = ["S", "O", "F", "T", "W", "A", "R", "E"];
   const line2 = ["D", "E", "V", "E", "L", "O", "P", "E", "R"];
 
   return (
@@ -24,102 +24,116 @@ const TitleCard = () => {
           times: [0, 0.315, 0.835, 0.84, 0.98, 0.99, 1],
         }}
       >
-        <div className="rowt">
-          <div className="dots"></div>
-          {line1.map((letter, i) => (
-            <div className="dots">
+        <div className="dots">
+          <div className="rowt">
+            {line1.map((letter, i) => (
               <motion.div
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ delay: 6 + i * 0.15, duration: 0.2 }}
-                className="dots"
+                className="starfont"
               >
-                <span>{letter}</span>
+                {letter}
               </motion.div>
-            </div>
-          ))}
-
-          <div className="dots"></div>
-        </div>
-        <div className="rowt">
-          <div className="dots"></div>
-          <div className="dots"></div>
-
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-          <div className="dots"></div>
-        </div>
-        <div className="rowt">
-          <div className="dots"></div>
-          {line2.map((letter, i) => (
-            <div className="dots">
+            ))}
+          </div>
+          <div className="rowlt">
+            <motion.div
+              className="name"
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                maxWidth: ["105px", "105px", "105px", "400px"],
+                overflow: ["hidden", "hidden", "hidden", "visible"],
+                opacity: [0, 1, 1, 1],
+              }}
+              transition={{
+                delay: 8.8,
+                ease: "easeIn",
+                times: [0, 0.3, 0.9, 1],
+                duration: 1.5,
+              }}
+            >
+              <motion.div
+                initial={{
+                  scale: 25,
+                  rotateY: 180,
+                  maxWidth: "105px",
+                  overflow: "hidden",
+                }}
+                animate={{
+                  scale: [25, 1, 1, 1],
+                  rotateY: [180, 0, 0, 0],
+                  maxWidth: ["105px", "105px", "105px", "400px"],
+                }}
+                transition={{
+                  delay: 8.8,
+                  duration: 1.2,
+                  ease: "easeIn",
+                  times: [0, 0.3, 0.9, 1],
+                }}
+                className="text"
+              >
+                HARI
+              </motion.div>
+            </motion.div>
+          </div>
+          <div className="rowmt">
+            <motion.div
+              className="name"
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                maxWidth: ["74px", "74px", "74px", "400px"],
+                overflow: ["hidden", "hidden", "hidden", "visible"],
+                opacity: [0, 1, 1, 1],
+              }}
+              transition={{
+                delay: 8.8,
+                ease: "easeIn",
+                times: [0, 0.3, 0.9, 1],
+                duration: 1.5,
+              }}
+            >
+              <motion.div
+                initial={{
+                  scale: 25,
+                  rotateY: 180,
+                  maxWidth: "74px",
+                  overflow: "hidden",
+                }}
+                animate={{
+                  scale: [25, 1, 1, 1],
+                  rotateY: [180, 0, 0, 0],
+                  maxWidth: ["74px", "74px", "74px", "400px"],
+                }}
+                transition={{
+                  delay: 8.8,
+                  duration: 1.2,
+                  ease: "easeIn",
+                  times: [0, 0.3, 0.9, 1],
+                }}
+                className="text"
+              >
+                HARI
+              </motion.div>
+            </motion.div>
+          </div>
+          <div className="rowt">
+            {line2.map((letter, i) => (
               <motion.div
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ delay: 7.5 + i * 0.15, duration: 0.2 }}
-                className="dots"
+                className="starfont"
               >
-                <span>{letter}</span>
+                {letter}
               </motion.div>
-            </div>
-          ))}
-          <div className="dots"></div>
+            ))}
+          </div>
         </div>
-        <motion.div
-          className="name"
-          initial={{
-            overflow: "hidden",
-            opacity: 0,
-            transform: "translate(-50%, -50%)",
-          }}
-          animate={{
-            transform: [
-              "translate(-50%, -50%)",
-              "translate(-50%, -50%)",
-              "translate(-50%, -50%)",
-              "translate(-50%, -50%)",
-            ],
-
-            maxWidth: ["84px", "84px", "84px", "400px"],
-            overflow: ["hidden", "hidden", "hidden", "visible"],
-            opacity: [0, 1, 1, 1],
-          }}
-          transition={{
-            delay: 8.8,
-            ease: "easeIn",
-            times: [0, 0.3, 0.9, 1],
-            duration: 1.5,
-          }}
-        >
-          <motion.div
-            initial={{
-              scale: 25,
-              rotateY: 180,
-              maxWidth: "105px",
-              overflow: "hidden",
-            }}
-            animate={{
-              scale: [25, 1, 1, 1],
-              rotateY: [180, 0, 0, 0],
-              maxWidth: ["84px", "84px", "84px", "400px"],
-            }}
-            transition={{
-              delay: 8.8,
-              duration: 1.2,
-              ease: "easeIn",
-              times: [0, 0.3, 0.9, 1],
-            }}
-            className="text"
-          >
-            HARI
-          </motion.div>
-        </motion.div>
       </motion.div>
       <motion.div
         className="introducing"
